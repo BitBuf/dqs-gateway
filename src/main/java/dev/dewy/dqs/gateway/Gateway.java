6765877114e1bdfdc71746dd8d36341f6301ea0f
Dewy <dewy@dewy.dev>
2020-06-24 16:44:29 +0100

    auth igc

--- src/main/java/dev/dewy/dqs/gateway/Gateway.java
+++ src/main/java/dev/dewy/dqs/gateway/Gateway.java
@@ -1,8 +1,23 @@
 package dev.dewy.dqs.gateway;
 
+import dev.dewy.dqs.gateway.utils.SessionHandler;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Plugin;
+
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class Gateway extends Plugin
 {
     public static Gateway INSTANCE;
+
+    public static HashMap<String, UUID> authTokenMap = new HashMap<>();
+    public static HashMap<String, ServerInfo> serverMap = new HashMap<>();
+
+    public static SessionHandler sessionHandler = new SessionHandler("DQSSessions.yml");
+    public static ScheduledExecutorService timerPool = Executors.newScheduledThreadPool(4);
+
+    public static String altCode;
 }
---
+++ src/main/java/dev/dewy/dqs/gateway/commands/game/AuthCommand.java
@@ -0,0 +1,48 @@
+package dev.dewy.dqs.gateway.commands.game;
+
+import dev.dewy.dqs.gateway.Gateway;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.plugin.Command;
+
+import java.util.Random;
+
+public class AuthCommand extends Command
+{
+    public AuthCommand()
+    {
+        super("auth");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args)
+    {
+        if (!(sender instanceof ProxiedPlayer))
+        {
+            sender.sendMessage(new TextComponent("\2477[\247b\247lDQS\247r\2477] \247fYou must be a proxied player to run this command."));
+
+            return;
+        }
+
+        if (!((ProxiedPlayer) sender).getServer().getInfo().getName().equalsIgnoreCase("center"))
+        {
+            sender.sendMessage(new TextComponent("\2477[\247b\247lDQS\247r\2477] \247fYou must be connected to the DQS gateway server to run this command."));
+
+            return;
+        }
+
+        Random random = new Random();
+        StringBuilder code = new StringBuilder();
+
+        for (int i = 0; i < 6; ++i)
+        {
+            code.append(random.nextInt(9));
+        }
+
+        Gateway.authTokenMap.put(code.toString(), ((ProxiedPlayer) sender).getUniqueId());
+
+        sender.sendMessage(new TextComponent("\2477[\247b\247lDQS\247r\2477] \247fHere's your six-digit code: \247l" + code.toString() + "\247r."));
+        sender.sendMessage(new TextComponent("\2477[\247b\247lDQS\247r\2477] \247fUse the &auth command in your \247lchannel\247rto authenticate."));
+    }
+}
